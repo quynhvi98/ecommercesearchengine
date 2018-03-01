1bb96a15440809da5c0559a6f067d8c118b70b99
viquy <viquy@192.168.1.117>
2018-03-01 09:02:44 +0700

    Create jsp& servlet for crud product

---
+++ src/java/com/webapp/controller/EditProductServlet.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webapp.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wepapp.beans.Product;
+import com.webapp.untils.MyUntils;
+import com.webapp.untils.DBUntils;
+import java.sql.Connection;
+
+/**
+ *
+ * @author viquy
+ */
+@WebServlet(urlPatterns = { "/editProduct" })
+public class EditProductServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    public EditProductServlet() {
+        super();
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EditProductServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EditProductServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Connection conn = MyUntils.getStoredConnection(request); 
+        String code = (String) request.getParameter("code");
+        Product product = null;
+        String errorString = null; 
+        try {
+            product = DBUntils.findProduct(conn, code);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errorString = e.getMessage();
+        }
+        if (errorString != null && product == null) {
+            response.sendRedirect(request.getServletPath() + "/productList");
+            return;
+        }
+        request.setAttribute("errorString", errorString);
+        request.setAttribute("product", product); 
+        RequestDispatcher dispatcher = request.getServletContext()
+                .getRequestDispatcher("/WEB-INF/views/editProductView.jsp");
+        dispatcher.forward(request, response);
+ 
+    }
+ 
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Connection conn = MyUntils.getStoredConnection(request); 
+        String code = (String) request.getParameter("code");
+        String name = (String) request.getParameter("name");
+        String priceStr = (String) request.getParameter("price");
+        float price = 0;
+        try {
+            price = Float.parseFloat(priceStr);
+        } catch (Exception e) {
+        }
+        Product product = new Product(code, name, price);
+ 
+        String errorString = null;
+ 
+        try {
+            DBUntils.updateProduct(conn, product);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errorString = e.getMessage();
+        }
+        request.setAttribute("errorString", errorString);
+        request.setAttribute("product", product);
+        if (errorString != null) {
+            RequestDispatcher dispatcher = request.getServletContext()
+                    .getRequestDispatcher("/WEB-INF/views/editProductView.jsp");
+            dispatcher.forward(request, response);
+        }
+        else {
+            response.sendRedirect(request.getContextPath() + "/productList");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
